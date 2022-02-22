2f2663e77994ec804b5aae8e847486ca110e04e3
Murren-Huang <smurren@163.com>
2022-02-22 14:54:01 +0800

    前端代码提交

---
+++ my-blog-ui/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+ 
+<template>
+<div id="app">
+    根节点，组件未更新 <br/>
+    首页 文章归档 搜索 关于我
+</div>
+</template>
+
+<style scoped>
+#app {
+    color: red;
+    font-size: 30px;
+}
+</style>
